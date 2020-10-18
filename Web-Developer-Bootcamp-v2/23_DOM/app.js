a68e97f052d94a707c4c65f68a6ed0c31995abac
Craig Maxwell <hello@craigmaxwell.me>
2020-10-18 18:59:50 +0200

    Intro to DOM

--- Web-Developer-Bootcamp-v2/23_DOM/app.js
+++ Web-Developer-Bootcamp-v2/23_DOM/app.js
@@ -23,3 +23,49 @@ document.querySelector(".square");
 document.querySelectorAll("h1");
 document.querySelectorAll("#red");
 document.querySelectorAll(".square");
+
+// manipulation
+// innerText - shows what is currently displaying in the webpage
+document.querySelector("p").innerText = "lolol";
+// textContent - shows everything in the object
+document.querySelector("p").textContent;
+
+const allLinks = document.querySelectorAll("a");
+for (let link of allLinks) {
+  link.innerText = "I am a link";
+}
+// innerHTML - gives entirety of markup contained within element
+document.querySelector("h1").innerHTML = "<sup>Superscript</sup>";
+
+// 247 - Attributes
+const firstLink = document.querySelector("a");
+// Get attributes in different ways - may return different results
+firstLink.href;
+firstLink.getAttribute("href");
+firstLink.href = "http://www.google.com";
+firstLink.setAttribute("href", "http://www.google.com");
+// manipulating styles - all css terms are camelCase
+// .style selector applies styles inline - not recommended
+// Instead can define css class using classList
+h1.classList.add("border");
+h1.classList.remove("border");
+h1.classList.toggle("border");
+// traversing parent/child/sibling
+// .parentElement - navigates to parent. every element has one direct element
+// .children - shows all childs of parent element
+// .nextElementSibling
+// .previousElementSibling
+// append and appendChild
+const newImage = document.createElement("img");
+newImage.src = "http://www.blahblahblah.com";
+document.body.appendChild(newImage);
+// also can use prepend
+// append - add text into something
+const par = document.querySelector("p");
+par.append("I am new text!!!");
+// .insertAdjacentElement
+
+// .removeChild() - old and inconvenient
+// better: .remove()
+const img = document.querySelector("img");
+img.remove();
