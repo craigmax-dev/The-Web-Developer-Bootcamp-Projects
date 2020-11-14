5bda5ebf5c786e67e72802e3d89089afc2f504e6
Craig Maxwell <hello@craigmaxwell.me>
2020-11-14 17:06:42 +0100

    Lesson 33 - Templating

--- Web-Developer-Bootcamp-v2/33_templating/app.js
+++ Web-Developer-Bootcamp-v2/33_templating/app.js
@@ -2,11 +2,23 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/r/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "Stephanie", "Winston"];
+  res.render("cats", { cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
 });
 
 app.get("/rand", (req, res) => {
